d8f3793be0e2fc62ca87e893d81ba1783a86dbc7
Hui <hui@digitalchaos.ca>
2021-09-29 16:47:37 -0400

    Init

---
+++ react-todo-app/src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>To Do List</h1>
+
+      <input type="checkbox" />
+      <p>Placeholder text here</p>
+
+      <input type="checkbox" />
+      <p>Placeholder text here</p>
+
+      <input type="checkbox" />
+      <p>Placeholder text here</p>
+
+      <input type="checkbox" />
+      <p>Placeholder text here</p>
+    </div>
+  );
+}
+
+export default App;
