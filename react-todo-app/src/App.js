0472b6a6b99e7690584ff065c46674044ce3ec1e
Hui <hui@digitalchaos.ca>
2021-10-07 16:22:29 -0400

    Updated components to stateful class based component

--- react-todo-app/src/App.js
+++ react-todo-app/src/App.js
@@ -2,24 +2,32 @@ import React from "react";
 import './App.css';
 
 import Greeting from "./components/Greeting"
-
 import TodoItem from './components/TodoItem';
-// import Joke from "./components/Joke";
-// import Product from "./components/Product";
+import LoginState from "./components/LoginState"
 
-// import jokesData from "./data/jokesData"
-// import productsData from "./data/vschoolProducts"
 import todosData from "./data/todosData"
 
 class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      todos: todosData
+    }
+  }
+
   render () {
-    const todoComponents = todosData.map(item => <TodoItem key={item.id} todoItem={item} />)
+    const todoComponents = this.state.todos.map(item => <TodoItem key={item.id} todoItem={item} />)
 
     return (
       <div>
         <div className="todo-list">
           <Greeting />
           {todoComponents}
+        </div>
+
+        <div>
+          <LoginState />
         </div>
       </div>
     )
