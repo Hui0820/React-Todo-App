4509ca85d4d0f34e32a8c8b538f66baadfffcbdb
Hui <hui@digitalchaos.ca>
2021-10-12 15:24:18 -0400

    Updated TodoItem.js

--- react-todo-app/src/components/TodoItem.js
+++ react-todo-app/src/components/TodoItem.js
@@ -4,7 +4,11 @@ class TodoItem extends React.Component {
   render() {
     return (
       <div className="todo-item">
-        <input type="checkbox" checked={this.props.todoItem.completed} onChange={() => console.log("Changed!")}/>
+        <input 
+          type="checkbox" 
+          checked={this.props.todoItem.completed} 
+          onChange={() => console.log("Changed!")}
+        />
         <p>{this.props.todoItem.text}</p>
       </div>
     )
